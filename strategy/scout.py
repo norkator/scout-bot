56cec590020d47245831782412c05f668a2816c3
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-05-04 21:34:47 +0300

    Add alarm sounds

--- strategy/scout.py
+++ strategy/scout.py
@@ -1,4 +1,5 @@
 from module import template_matcher, frame_capture, move_mouse, random_utils
+from playsound import playsound
 import pyautogui
 import time
 import sys
@@ -15,6 +16,7 @@ STATE_IN_CAVE = 8
 STATE_CAVE_DETECT_OPTIMAL_RAID = 9
 STATE_CAVE_ALARM = 10
 STATE_CAVE_EXIT = 11
+STATE_CAVE_FINISHED = 11
 KILL_PROCESS = 666
 
 OPTIMAL_RAIDS = [
@@ -84,7 +86,7 @@ def scout(game):
             game.__setstate__(STATE_IN_CAVE)
 
     elif game.get_state() is STATE_IN_CAVE:
-        print('We have eneterd in the cave')
+        print('We have entered in the cave')
         game.__setstate__(STATE_CAVE_DETECT_OPTIMAL_RAID)
 
     elif game.get_state() is STATE_CAVE_DETECT_OPTIMAL_RAID:
@@ -92,13 +94,39 @@ def scout(game):
             window_frame, 'find_raid', OPTIMAL_RAIDS, game.get_state(), plot=True
         )
         if match_found is True:
-            game.__setstate__(KILL_PROCESS)  # Todo, alarm here
+            game.__setstate__(STATE_CAVE_ALARM)
         else:
             game.__setstate__(STATE_CAVE_EXIT)
 
     elif game.get_state() is STATE_CAVE_EXIT:
+        # this tries to calculate position to leave cave
+        x_p = int(((game.x2 / 2) + game.x) * 60 / 100)
+        y_p = int(((game.y2 / 2) + game.y) * 99 / 100)
+        move_mouse.random_mouse_move(x_p, y_p, rnd=400, duration=0.5)
+        pyautogui.click()
+        time.sleep(2)
+        window_frame = frame_capture.capture_window_frame(game.x, game.y, game.x2, game.y2, im_show=False)
+        tp = template_matcher.feature_matcher(window_frame, 'leave_cave.png', game.get_state())
+        target_offset_x, target_offset_y = random_utils.random_point(
+            tp[0] + game.x, tp[1] + game.y, tp[2] + game.x, tp[3] + game.y
+        )
+        move_mouse.random_mouse_move(target_offset_x, target_offset_y, rnd=400, duration=0.5)
+        pyautogui.click()
         time.sleep(5)
         game.__setstate__(STATE_BEGINNING)
+
+    elif game.get_state() is STATE_CAVE_ALARM:
+        bell = os.getcwd() + '/sounds/' + 'bell.wav'
+        playsound(bell)
+        playsound(bell)
+        playsound(bell)
+        alert = os.getcwd() + '/sounds/' + 'alert.wav'
+        playsound(alert)
+        game.__setstate__(STATE_CAVE_FINISHED)
+
+    elif game.get_state() is STATE_CAVE_FINISHED:
+        print('this window is in finished state')
+        # Todo, maybe ad key to reset this?
 
     elif game.get_state() is KILL_PROCESS:
         sys.exit(0)
@@ -125,6 +153,8 @@ def scout_test(game):
     # template_matcher.feature_matcher(window_frame, 'make_party.png', game.get_state(), plot=True)
 
     # template_matcher.feature_matcher_match_found(window_frame, 'find_raid', ['raid_1.png'], game.get_state(), plot=True)
+
+    # template_matcher.feature_matcher(window_frame, 'leave_cave.png', game.get_state(), plot=True)
 
 
 def cave_enter_success(game):
